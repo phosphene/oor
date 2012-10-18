a1e37efeb383cfe5e8518a0edc0b0447955fc8eb
Cynthia Kiser <cnk@caltech.edu>
2012-10-17 19:05:01 -0700

    Added publication date to posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,13 +1,14 @@
 class Post
   extend ActiveModel::Naming
   include ActiveModel::Conversion
-  attr_accessor :title, :body, :blog
+  attr_accessor :title, :body, :blog, :pubdate
 
   def initialize(attrs={})
     attrs.each do |k,v| send("#{k}=", v) end
   end
 
-  def publish
+  def publish(clock=DateTime)
+    self.pubdate = clock.now
     blog.add_entry(self)
   end
 
