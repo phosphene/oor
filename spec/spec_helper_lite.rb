86a2c888ce9b61f4841c64de25f94e79b8a22607
Cynthia Kiser <cnk@caltech.edu>
2012-10-21 23:56:49 -0700

    stub_class method from the book

--- spec/spec_helper_lite.rb
+++ spec/spec_helper_lite.rb
@@ -1,9 +1,30 @@
-def stub_module(full_name) 
+def stub_module(full_name, &block) 
+  stub_class_or_module(full_name, Module)
+end
+
+def stub_class(full_name, &block) 
+  stub_class_or_module(full_name, Class)
+end
+
+def stub_class_or_module(full_name, kind, &block)
   full_name.to_s.split(/::/).inject(Object) do |context, name|
     begin
+      # First try to autoload the class or module
       context.const_get(name) 
     rescue NameError
-      context.const_set(name, Module.new) 
+      # Overload const_missing
+      mod = Module.new do
+        define_method(:const_missing) do |missing_constant_name|
+          if missing_constant_name.to_s == name.to_s
+            value = kind.new
+            const_set(name, value)
+            value
+          else
+            super(missing_const_name)
+          end
+        end
+      end
+      context.extend(mod)
     end
   end 
 end
